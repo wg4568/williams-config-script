10fe088adf2aa5f8bed8f8732223f164c2f09e1f
William Gardner <william@madebycounter.com>
2024-01-13 21:00:30 -0800

    Added checks for existing directory

---
+++ src/preinstall.sh
@@ -0,0 +1,15 @@
+if test -d $WORKING_DIR; then
+    read -p "$WORKING_DIR exists, remove it? (yes/no) " -n 1 -r
+    echo
+
+    if [[ ! $REPLY =~ ^[Yy]$ ]]
+    then
+        echo "Exiting."
+        exit 0
+    fi
+
+    rm -rf $WORKING_DIR
+    echo "Deleted." 
+fi
+
+mkdir -p $WORKING_DIR
