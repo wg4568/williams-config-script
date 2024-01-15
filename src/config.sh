7a4dc4486b7a73da9c5ac3eb958338491ad27a31
William Gardner <william@madebycounter.com>
2024-01-14 17:28:00 -0800

    Added speedtest-cli

--- src/config.sh
+++ src/config.sh
@@ -1,12 +1,12 @@
 WORKING_DIR="/tmp/williams-config-script"
 
 APT_PACKAGES="
-    curl git unzip gnupg
+    curl unzip gnupg
     wireguard tmux neofetch
     gparted conky uxplay transmission-qt
     cifs-utils keyutils kio-extras openresolv
-    python3 python3-pip gh
-    linux-wifi-hotspot
+    git gh python3 python3-pip
+    linux-wifi-hotspot speedtest-cli
 "
 
 GIT_REPOS="
