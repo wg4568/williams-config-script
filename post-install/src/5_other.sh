b1dbf956e1f89ec1e952b48162f5356e17a2c1e9
William Gardner <william@madebycounter.com>
2024-01-27 16:36:32 -0800

    Added material decoration

--- post-install/src/5_other.sh
+++ post-install/src/5_other.sh
@@ -1,5 +1,6 @@
 ln -s /mnt/counter ~/counter
 sudo systemctl enable sddm
+yes | yay -Sy material-kwin-decoration-git
 read -p "Would you like to install nvidia and alsa firmware? [y/n] " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]] then
